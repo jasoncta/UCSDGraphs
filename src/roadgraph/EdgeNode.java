a306464cae32636e36098b94c274c421088cad5a
Jason Tan <jasoncta@usc.edu>
2016-06-11 15:46:31 -0700

    Add comments

--- src/roadgraph/EdgeNode.java
+++ src/roadgraph/EdgeNode.java
@@ -2,14 +2,17 @@ package roadgraph;
 
 import geography.GeographicPoint;
 
+// This class stores all information for the edges
 public class EdgeNode {
 	
+	// Variables for an edgeNode
 	private GeographicPoint from;
 	private GeographicPoint to;
 	private String roadName;
 	private String roadType;
 	private double length;
 	
+	// Constructor
 	public EdgeNode(GeographicPoint from, GeographicPoint to, String roadName,
 			String roadType, double length) {
 		this.from = from;
@@ -20,14 +23,17 @@ public class EdgeNode {
 		
 	}
 	
+	// Gets the start-node location
 	public GeographicPoint getFrom() {
 		return this.from;
 	}
 	
+	// Gets the end-node location
 	public GeographicPoint getTo() {
 		return this.to;
 	}
 	
+	// Converts object to readable string
 	public String toString() {
 		String s = "";
 		s += "[From: " + this.from + " To: " + this.to + " RoadName: " + this.roadName + 
